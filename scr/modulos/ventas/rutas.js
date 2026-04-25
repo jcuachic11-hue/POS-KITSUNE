b49454743c43d69b4f39d0e965c402ad628e4bf7
jean <j.cuachic11@gmail.com>
2026-04-25 17:57:31 -0600

    ventas cookies

--- scr/modulos/ventas/rutas.js
+++ scr/modulos/ventas/rutas.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const controlador = require('./controlador');
 
 // RUTAS
-router.post('/carrito', controlador.agregarCarrito);
-router.get('/carrito', controlador.listarCarrito);
-router.get('/totales', controlador.totalesCarrito);
-router.post('/comprar', controlador.comprar);
+router.post('/carrito', verificarMiddleware, controlador.agregarCarrito);
+router.get('/carrito', verificarMiddleware, controlador.listarCarrito);
+router.get('/totales', verificarMiddleware, controlador.totalesCarrito);
+router.post('/comprar', verificarMiddleware, controlador.comprar);
 
 module.exports = router; // 

fa884b8d4dc0254943214976cddb3c63b786d992
jean <j.cuachic11@gmail.com>
2026-04-25 18:24:09 -0600

    ventas cookies correccion

--- scr/index.js
+++ scr/index.js
@@ -18,6 +18,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cookieParser());
+
 
 app.use(express.static(path.join(__dirname, 'publico')));
 

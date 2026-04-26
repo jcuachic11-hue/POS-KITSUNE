5a6bfe7a31e2edce12eee9745153352b69f88b03
jean <j.cuachic11@gmail.com>
2026-04-25 22:00:47 -0600

    ventas cookies22

--- scr/index.js
+++ scr/index.js
@@ -1,44 +1,13 @@
+// IMPORTACIONES 
 const express = require('express');
 const cors = require('cors');
-const {verificador} = require ('../') 
-
-
 const path = require('path');
-
-const cookieParser = require ('cookie-parser');
-
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 const app = express();
 
-const session = require('express-session'); //para captcha
-
-// --- MIDDLEWARES ---
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(cookieParser());
-
-// --- ENDPOINTS ---
-app.use('/productos', productos);
-app.use('/usuarios', usuarios);
-app.use('/ventas', ventas);
-app.use('/devoluciones', devoluciones);
-app.use('/login', authRutas);
-app.use('/promociones', promociones);
-
-app.use(express.static(path.join(__dirname, 'publico')));
-
-app.use(session({ //captcha 
-
-    // Si process.env.SESSION_SECRET no existe, usa la frase de la derecha
-    secret: process.env.SESSION_SECRET || 'clave_de_emergencia_kitsune_123', 
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false }
-}));
-
+// DEFINIR LAS RUTAS
 const productos = require('./modulos/productos/rutas');
 const usuarios = require('./modulos/usuarios/rutas');
 const ventas = require('./modulos/ventas/rutas');
@@ -46,10 +15,24 @@ const devoluciones = require('./modulos/devoluciones/rutas');
 const authRutas = require('./modulos/auth/rutas');
 const promociones = require('./modulos/promociones/rutas');
 
+// MIDDLEWARES
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser()); // <--- ¡Asegúrate de que esté aquí!
 
 
-// --- PUERTO ---
+app.use('/productos', productos);
+app.use('/usuarios', usuarios);
+app.use('/ventas', ventas);
+app.use('/devoluciones', devoluciones);
+app.use('/login', authRutas);
+app.use('/promociones', promociones);
+
+
+app.use(express.static(path.join(__dirname, 'publico')));
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(` Servidor POS Kitsune corriendo en puerto ${PORT}`);
+    console.log(`Servidor POS Kitsune corriendo en puerto ${PORT}`);
 });

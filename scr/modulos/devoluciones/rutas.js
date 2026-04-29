dc5cf679342c0fa9492c875ff8962aee1941d5d8
jean <j.cuachic11@gmail.com>
2026-04-29 00:07:41 -0600

    devoluciones cookies

--- scr/modulos/devoluciones/rutas.js
+++ scr/modulos/devoluciones/rutas.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const controlador = require('./controlador');
 const { verificarMiddleware } = require('../admin'); 
 
-router.use(verificarMiddleware);
+router.use(verificarMiddleware); 
+
 
 router.post('/', controlador.devolverProducto); 
 router.get('/', controlador.listarDevoluciones);

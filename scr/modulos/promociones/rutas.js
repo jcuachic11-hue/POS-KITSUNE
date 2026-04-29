9935bd8d237c965418cb08ab8b35e13fd31fab44
jean <j.cuachic11@gmail.com>
2026-04-29 01:04:54 -0600

    promociones  cookies

--- scr/modulos/promociones/rutas.js
+++ scr/modulos/promociones/rutas.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const controlador = require('./controlador');
-// const auth = require('../../auth'); 
+const { verificarMiddleware } = require('../../admin'); 
+
+
+router.use(verificarMiddleware);
 
 router.get('/', controlador.listarPromociones);
-router.get('/:codigo', controlador.obtenerPromocion); // Para buscar en ventas
+router.get('/:codigo', controlador.obtenerPromocion); 
 router.post('/', controlador.crearPromocion);
 router.delete('/:id', controlador.eliminarPromocion);
 router.put('/', controlador.actualizarPromocion);
